7a6615aebc6aceb1e9d18a865033798f91aba32d
Jacob Eiferman <jacobeiferman@gmail.com>
2022-05-12 15:05:30 -0400

    Python DS syntax exercise

---
+++ projects/18.2 python-ds-practice/01_product/product.py
@@ -0,0 +1,14 @@
+def product(a, b):
+    """Return product of a and b.
+
+        >>> product(2, 2)
+        4
+
+        >>> product(2, -2)
+        -4
+    """
+    return a * b
+
+    #2, 7, 17, 23, 26, 32, 34, 35
+    #25, 33
+    
