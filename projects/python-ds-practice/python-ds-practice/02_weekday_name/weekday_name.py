f30f9878c0a9c8e3b1e031c0853728c3e52c8508
Jacob Eiferman <jacobeiferman@gmail.com>
2025-12-10 16:22:03 -0500

    add exercise #2

--- projects/python-ds-practice/python-ds-practice/02_weekday_name/weekday_name.py
+++ projects/python-ds-practice/python-ds-practice/02_weekday_name/weekday_name.py
@@ -12,3 +12,17 @@ def weekday_name(day_of_week):
         >>> weekday_name(9)
         >>> weekday_name(0)
     """
+    days_of_week = {
+        1: 'Sunday',
+        2: 'Monday',
+        3: 'Tuesday',
+        4: 'Wednesday',
+        5: 'Thursday',
+        6: 'Friday',
+        7: 'Saturday'
+    }
+
+    if day_of_week in days_of_week.keys():
+        return days_of_week.get(day_of_week)
+    return None
+   
