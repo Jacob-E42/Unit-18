98e4d68a4dbebbee98c1d90fc740f43ab1d27b04
Jacob Eiferman <jacobeiferman@gmail.com>
2025-12-18 16:15:43 -0500

    take out return lines

--- projects/python-ds-practice/python-ds-practice/08_list_manipulation/list_manipulation.py
+++ projects/python-ds-practice/python-ds-practice/08_list_manipulation/list_manipulation.py
@@ -47,14 +47,12 @@ def list_manipulation(lst, command, location, value=None):
         elif location == "end":
             lst.append(value)
             return lst
-        else:
-            return None
+        
     elif command == "remove":
         if location == "beginning":
             return lst.pop(0)
         elif location == "end":
             return lst.pop()
-        else:
-            return None
+       
 
     return None
