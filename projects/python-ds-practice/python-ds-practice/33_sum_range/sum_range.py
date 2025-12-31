ee68eb5696dc1420a026787c04afa399305c698b
Jacob Eiferman <jacobeiferman@gmail.com>
2025-12-31 16:29:19 -0500

    add sum range exercise

--- projects/python-ds-practice/python-ds-practice/33_sum_range/sum_range.py
+++ projects/python-ds-practice/python-ds-practice/33_sum_range/sum_range.py
@@ -23,3 +23,9 @@ def sum_range(nums, start=0, end=None):
         >>> sum_range(nums, 1, 99)
         9
     """
+    if end is None:
+        end = len(nums)
+        
+    to_sum = nums[start:end +1 ]
+
+    return sum(to_sum)
