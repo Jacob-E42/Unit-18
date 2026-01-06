beab8e9534fa06027add6c4827338f72c5e04768
Jacob Eiferman <jacobeiferman@gmail.com>
2026-01-06 18:08:03 -0500

    add read file list exercise

--- projects/python-ds-practice/python-ds-practice/fs_5_read_file_list/read_file_list.py
+++ projects/python-ds-practice/python-ds-practice/fs_5_read_file_list/read_file_list.py
@@ -19,3 +19,8 @@ def read_file_list(filename):
     # hint: when you read lines of files, there will be a "newline"
     # (end-of-line character) at the end of each line, and you want to
     # strip that off before you print it. Do some research on that!
+    # import os
+    # print(os.getcwd())
+    with open(filename) as f:
+        for line in f:
+            print("-", line.rstrip("\n"))
